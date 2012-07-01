04560bd665e44d50bf1c165dbea8ee56b63c664f
Brett Slatkin <bslatkin@gmail.com>
2012-07-01 13:46:47 -0700

    got it working on some publisher sites

--- find_buttons.js
+++ find_buttons.js
@@ -1,31 +1,32 @@
 // This is all fake config that should be in local storage.
 var config = {
   'http://www.blogger.com/': {
-    'template': 'http://www.blogger.com/blog-this.g?t={summary}&n={title}&u={url}',
-    'prettyName': 'Blogger'
+    'template': 'http://www.blogger.com/blog-this.g?n={title}&u={url}',
+    'prettyName': 'Blogger',
+    'icon': 'http://www.blogger.com/favicon.ico'
   }
 };
 var serviceId = 'http://www.blogger.com/';
 var serviceConfig = config[serviceId];
 
 
-function templatizeUrl(template, summary, title, url) {
+function templatizeUrl(template, title, url) {
   return template.
-    replace(/\{summary\}/g, encodeURIComponent(summary)).
     replace(/\{title\}/g, encodeURIComponent(title)).
     replace(/\{url\}/g, encodeURIComponent(url));
 }
 
 
-function createButton(parent) {
+function createButton(parent, title, url) {
   var el = document.createElement('a');
-  var dest = templatizeUrl(
-      serviceConfig.template,
-      'fake summary',
-      'fake title',
-      'http://www.example.com');
+  var dest = templatizeUrl(serviceConfig.template, title, url);
   el.setAttribute('href', dest);
   el.setAttribute('target', '_blank');
+  el.setAttribute('class', 'interceptor-button');
+  el.setAttribute(
+      'style',
+      'background-image: url(' + serviceConfig.icon + '), ' +
+      '-webkit-linear-gradient(#efefef, #e0e0e0);');
   el.innerText = serviceConfig.prettyName;
   parent.appendChild(el);
 }
@@ -33,11 +34,9 @@ function createButton(parent) {
 
 function handleDomRemove(e) {
   var target = e.target;
-
   if (!target) {
     return;
   }
-
   if (target.nodeType != 1) {
     return;
   }
@@ -48,15 +47,21 @@ function handleDomRemove(e) {
 
   // Try to identify the Twitter button's markup.
   if (target.href && target.href.indexOf('http://twitter.com/share?') == 0) {
-    createButton(target.parentNode);
+    createButton(target.parentNode,
+        target.getAttribute('data-text'),
+        target.getAttribute('data-url'));
   }
 }
 
 
+function stopListening() {
+  document.removeEventListener("DOMNodeRemoved", handleDomRemove, true);
+}
+
+
 function init() {
-  document.addEventListener(
-      "DOMNodeRemoved", handleDomRemove, true);
-  // TODO: On body finished load event, stop listening.
+  document.addEventListener("DOMNodeRemoved", handleDomRemove, true);
+  window.addEventListener("load", stopListening);
 }
 
 
